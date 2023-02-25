fcfa24d8a99d00b9aa502a874a07dbae96dd155b
Swabhan Katkoori <swabhankatkoor@icloud.com>
2023-02-25 15:19:21 -0600

    Working Screens and Buttons

--- App.js
+++ App.js
@@ -1,22 +1,111 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
+// import { Picker } from '@react-native-picker/picker';
+import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from '@react-navigation/native';
 
-export default function App() {
+
+const HomeScreen = ({ navigation }) => {
   return (
     <View style={styles.container}>
-      <Text>afdadfad
-        
-      </Text>
-      <StatusBar style="auto" />
+      <Text style={styles.title}>Home Screen</Text>
+      <Button
+        title="User"
+        onPress={() => navigation.navigate('User')}
+      />
     </View>
   );
 }
 
+
+//----------
+//User
+//----------
+
+UserScreen = ({ navigation }) => {
+  return (
+      <View style={styles.container}>
+        <Button 
+          title="Report Disaster"
+          onPress={() => navigation.navigate('Report')}
+        />
+
+        <Button 
+          title="Call Emergency Services"
+          onPress={() => navigation.navigate('Call')}
+        />
+
+        <Button 
+          title="Emergency Contacts"
+          onPress={() => navigation.navigate('Contact')}
+        />
+      </View>
+
+      
+  );
+}
+
+const ReportScreen = ({ navigation }) => {
+  return(
+    <View style={styles.container}>
+        <Text style={styles.title}>Report</Text>
+    </View>
+  );
+      
+}
+
+CallScreen = () => {
+  return (
+      <View style={styles.container}>
+      <Text style={styles.title}>Call Emergency Services</Text>
+      </View>
+  );
+}
+
+ContactScreen = () => {
+  return (
+      <View style={styles.container}>
+      <Text style={styles.title}>Emergency Contacts</Text>
+      </View>
+  );
+}
+
+
+//----------
+//Navigation
+//----------
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen}/>
+        <Stack.Screen name="User" component={UserScreen}/>
+        <Stack.Screen name="Report" component={ReportScreen}/>
+        <Stack.Screen name="Call" component={CallScreen}/>
+        <Stack.Screen name="Contact" component={ContactScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+//-------------
+//Geo-location
+//-------------
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 20,
   },
 });
+
+export default App;
